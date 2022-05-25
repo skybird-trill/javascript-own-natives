bf674f79ab1e08a7cf2987b24f245fe58da8170d
Natalia Bakhareva <skybird-trill@users.noreply.github.com>
2022-05-25 15:15:23 +0500

    Create bind-in-prototype.js

---
+++ bind-in-prototype.js
@@ -0,0 +1,67 @@
+// use native bind in prototype
+Function.prototype.myBind = function(context, ...args) {
+  return this.bind(context, ...args);
+};
+
+// use native apply in prototype
+Function.prototype.myBind = function(context, ...args) {
+  const boundFunction = this;
+  return function (...bindArgs) {
+    boundFunction.apply(context, args.concat(bindArgs));
+  };
+};
+
+// use native call in prototype
+Function.prototype.myBind = function(context, ...args) {
+  const boundFunction = this;
+  return function (...bindArgs) {
+    boundFunction.call(context, ...args.concat(bindArgs));
+  };
+};
+
+// create new context in prototype
+Function.prototype.myBind = function(context, ...args) {
+  const boundFunction = this;
+  return function (...bindArgs) {
+    const uniqFieldName = Symbol("bindFunction");
+    const boundContext = {
+      ...context,
+      [uniqFieldName]: boundFunction,
+    };
+    boundContext[uniqFieldName](...args.concat(bindArgs));
+  };
+};
+
+// modify current context in prototype
+Function.prototype.myBind = function(context, ...args) {
+  const boundFunction = this;
+  return function (...bindArgs) {
+    const uniqFieldName = Symbol("bindFunction");
+    context[uniqFieldName] = boundFunction;
+    context[uniqFieldName](...args.concat(bindArgs));
+    delete context[uniqFieldName];
+  };
+};
+
+// es5 with native apply in prototype
+Function.prototype.myBind = function(context) {
+  const args = Array.prototype.slice.call(arguments, 1);
+  const boundFunction = this;
+  return function(argunemts) {
+    const bindArgs = Array.prototype.slice.call(arguments);
+    boundFunction.apply(context, args.concat(bindArgs));
+  };
+};
+
+// es5 modify current context in prototype
+Function.prototype.myBind = function(context) {
+  const args = Array.prototype.slice.call(arguments, 1);
+  const boundFunction = this;
+  return function(argunemts) {
+    const bindArgs = Array.prototype.slice.call(arguments);
+    const uniqFieldName = Date.now().toString();
+    context[uniqFieldName] = boundFunction;
+    context[uniqFieldName].apply(context, args.concat(bindArgs));
+    delete context[uniqFieldName];
+  };
+};
